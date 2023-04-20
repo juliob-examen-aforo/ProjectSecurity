766c23a19d2e80fa93a63e30ce023e64f52693c7
Julio Cesar Becerra Urbina <julioc_m18@hotmail.com>
2023-04-20 08:50:56 -0500

    add SEQ to app security

--- src/app/controllers/auth.controller.js
+++ src/app/controllers/auth.controller.js
@@ -1,20 +1,27 @@
-const authService = require('../services/auth.service')
-const jwt = require('jsonwebtoken');
-const logProvider = require('../middlewares/logprovider')
+const authService = require("../services/auth.service");
+const jwt = require("jsonwebtoken");
+const logProvider = require("../middlewares/logprovider");
 
 const getUsersByCredentials = async (req, res) => {
-    logProvider.info('Start getUsersByCredentials in auth.controller.js')
+  logProvider.info("Start getUsersByCredentials in auth.controller.js");
 
-    const { userName, password } = req.body
-    const result = await authService.getUsersByCredentials(userName, password);
-    console.log(result);
-    if(result.length > 0)
-    {
-        const token = jwt.sign({ username: userName }, process.env.TOKEN_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION })
-        return res.status(200).json({ token: token, username: userName, expiration: process.env.TOKEN_EXPIRATION })
-    }
-    else
-        return res.status(401).send({ error: 'Unauthorized user' });
-}
+  const { userName, password } = req.body;
+  const result = await authService.getUsersByCredentials(userName, password);
 
-module.exports = { getUsersByCredentials }
+  if (result.length > 0) {
+    logProvider.info("Loggin successful", result);
+    const token = jwt.sign({ username: userName }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    });
+    return res.status(200).json({
+      token: token,
+      username: userName,
+      expiration: process.env.TOKEN_EXPIRATION,
+    });
+  } else {
+    logProvider.info("Loggin failed", result);
+    return res.status(401).send({ error: "Unauthorized user" });
+  }
+};
+
+module.exports = { getUsersByCredentials };
